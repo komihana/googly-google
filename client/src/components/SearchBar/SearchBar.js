d7dd61d3106a0485cea982f840e984815f489d56
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-16 19:32:12 -0400

    repurposed books activity from week 20

---
+++ client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBah() {
+  return (
+    <div>
+    <input type="text" className="input" placeholder="Search..." />
+    </div>
+  );
+}
+
+export default SearchBah;
