94ef0dacf455a533ba39dfaae94ba6d5796dcb83
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-16 23:35:37 -0400

    ran npm build

---
+++ client/build/precache-manifest.4e7584201cd653c43f87db17f4070d42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "124aa69bbb3f326f3298",
+    "url": "/static/js/main.579b963e.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "124aa69bbb3f326f3298",
+    "url": "/static/css/main.bc98d83a.chunk.css"
+  },
+  {
+    "revision": "5f9a009b5af0c4a949545fce85124062",
+    "url": "/index.html"
+  }
+];
