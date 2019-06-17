44ec17f66d8f29a22652d4b8c99e3155a14a839d
Han Seoulo <cu.rogue.coder@gmail.com>
2019-06-16 22:06:51 -0400

    fixed server

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(MONGODB_URI);
 
 // Start the API server
 app.listen(PORT, function() {
